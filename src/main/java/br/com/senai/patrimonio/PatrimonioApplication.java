b526a0d23d3f3e4b712d60b827ab79946942b9bb
Prado <ezequias.moreira.do.prado@gmail.com>
2026-08-24 22:03:19 -0300

    cria classe Bem, cargo e Funcionario

--- src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
+++ src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
@@ -2,6 +2,9 @@ package br.com.senai.patrimonio;
 
 import br.com.senai.patrimonio.model.Empresa;
 import br.com.senai.patrimonio.model.Endereco;
+import br.com.senai.patrimonio.model.Funcionario;
+import br.com.senai.patrimonio.model.Sala;
+import br.com.senai.patrimonio.model.enums.Cargo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -29,7 +32,14 @@ public class PatrimonioApplication {
 				"Santa luzia","Criciúma", "SC");
 		System.out.println(enderecoComArgumentos.getBairro());
 
+		Sala sala = new Sala();
 
+		Funcionario funcionario = new Funcionario(
+				35L,"Mariazinha","13456789",
+				Cargo.GERENTE, empresa, sala
+		);
+
+		System.out.println(funcionario.getCpf());
 
 
 	}
